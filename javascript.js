d366be71f5d240ecdc785cb60687c636ddfa58c3
Iqra <iqrayasir2609@gmail.com>
2023-05-02 21:45:12 +0100

    Write some of if statement and fix syntax error

--- javascript.js
+++ javascript.js
@@ -12,6 +12,19 @@ function getComputerChoice(randomNumber) {
     }
 }
 
+function playRound(playerSelection,computerSelection) {
+    let lowerCase=playerSelection.toLowerCase();
+    if (lowerCase==="rock") {
+        if (computerSelection==="rock") {
+            return "We tie! Rock equals rock";
+        } else if (computerSelection==="paper") {
+            return "You lose! Paper beats rock";
+        } else {
+            return "You win! Rock beats scissor"
+        }
+    }
+} 
+
 let randomNumber=Math.floor(Math.random()*3)+1;
-let playerSelection = prompt("Pick one: rock, paper or scissors? ")
+let playerSelection = prompt("Pick one: rock, paper or scissors? ");
 let computerSelection=getComputerChoice(randomNumber);
