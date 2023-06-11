6c6179bb196c77a0837e0531d4206738392ca9ef
Iqra <iqrayasir2609@gmail.com>
2023-06-11 19:21:50 +0100

    Create div elements for dialogue

--- javascript.js
+++ javascript.js
@@ -15,37 +15,38 @@ function getComputerChoice(randomNumber) {
 
 function playRound(playerSelection,computerSelection) {
     let lowerCase = playerSelection.toLowerCase();
+    const declaration = document.createElement('div');
     if (lowerCase==="rock") {
         if (computerSelection==="rock") {
-            console.log("We tie! Rock equals rock");
+            declaration.textContent = "We tie! Rock equals rock";
             return 2;
         } else if (computerSelection==="paper") {
-            console.log("You lose! Paper beats rock");
+            declaration.textContent = "You lose! Paper beats rock";
             return 0;
         } else {
-            console.log("You win! Rock beats scissors");
+            declaration.textContent = "You win! Rock beats scissors";
             return 1;
         }
     } else if (lowerCase==="paper") {
         if (computerSelection==="paper") {
-            console.log("We tie! Paper equals paper");
+            declaration.textContent = "We tie! Paper equals paper";
             return 2;
         } else if (computerSelection==="scissors") {
-            console.log("You lose! Scissors beats paper");
+            declaration.textContent = "You lose! Scissors beats paper";
             return 0;
         } else {
-            console.log("You win! Paper beats rock");
+            declaration.textContent = "You win! Paper beats rock";
             return 1;
         }
     } else {
         if (computerSelection==="scissors") {
-            console.log("We tie! Scissors equals scissors");
+            declaration.textContent = "We tie! Scissors equals scissors";
             return 2;
         } else if (computerSelection==="rock") {
-            console.log("You lose! Rock beats scissors");
+            declaration.textContent = "You lose! Rock beats scissors";
             return 0;
         } else {
-            console.log("You win! Scissors beats paper");
+            declaration.textContent = "You win! Scissors beats paper";
             return 1;
         }
     }
@@ -53,29 +54,30 @@ function playRound(playerSelection,computerSelection) {
 
 function game() {
     let randomNumber=Math.floor(Math.random()*3)+1;
-    let playerSelection = prompt("Pick one: rock, paper or scissors? ");
+    let playerSelection = 
     let computerSelection = getComputerChoice(randomNumber);
     let winningNumber=playRound(playerSelection,computerSelection);
     return winningNumber;
 }
 
 //main program
-let player_score = 0
-let computer_score = 0
+let player_score = 0;
+let computer_score = 0;
 
 winningNumber=game();
+const point = document.createElement('div');
 if (winningNumber===0) {
     computer_score++
-    console.log("Your opponent gains a point")
+    point.textContent ="Your opponent gains a point\n";
 }
 else if (winningNumber===1) {
     player_score++
-    console.log("You gain a point")
+    point.textContent ="You gain a point\n";
 } else {
-    console.log("No one gains a point")
+    point.textContent ="No one gains a point\n"
 }
 
 let result = (player_score<computer_score)
  ? "You lost! Try again?" 
  : "You won! Rematch?"
-console.log(result)
+winner.textContent = `${result}`
