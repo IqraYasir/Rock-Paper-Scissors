2bf33b17e122500de1fda63a1fab0825f8831d12
Iqra <iqrayasir2609@gmail.com>
2023-06-12 15:15:26 +0100

    Append divs and fix player selection bugs

--- javascript.js
+++ javascript.js
@@ -19,57 +19,68 @@ function playRound(playerSelection,computerSelection) {
     if (lowerCase==="rock") {
         if (computerSelection==="rock") {
             declaration.textContent = "We tie! Rock equals rock";
+            declaration.appendChild(container);
             return 2;
         } else if (computerSelection==="paper") {
             declaration.textContent = "You lose! Paper beats rock";
+            declaration.appendChild(container);
             return 0;
         } else {
             declaration.textContent = "You win! Rock beats scissors";
+            declaration.appendChild(container);
             return 1;
         }
     } else if (lowerCase==="paper") {
         if (computerSelection==="paper") {
             declaration.textContent = "We tie! Paper equals paper";
+            declaration.appendChild(container);
             return 2;
         } else if (computerSelection==="scissors") {
             declaration.textContent = "You lose! Scissors beats paper";
+            declaration.appendChild(container);
             return 0;
         } else {
             declaration.textContent = "You win! Paper beats rock";
+            declaration.appendChild(container);
             return 1;
         }
     } else {
         if (computerSelection==="scissors") {
             declaration.textContent = "We tie! Scissors equals scissors";
+            declaration.appendChild(container);
             return 2;
         } else if (computerSelection==="rock") {
             declaration.textContent = "You lose! Rock beats scissors";
+            declaration.appendChild(container);
             return 0;
         } else {
             declaration.textContent = "You win! Scissors beats paper";
+            declaration.appendChild(container);
             return 1;
         }
     }
 } 
 
-function game(e) {
-    const button = e.target;
+function game(playerSelection) {
     let randomNumber = Math.floor(Math.random()*3)+1;
-    let playerSelection = button.getAttribute('id');
+    console.log(playerSelection)
     let computerSelection = getComputerChoice(randomNumber);
     let winningNumber=playRound(playerSelection,computerSelection);
-    return winningNumber;
 }
 
 //main program
 let playerScore = 0;
 let computerScore = 0;
+const container = document.querySelector('.container');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', game);
-});
-
-winningNumber=game();
+    button.addEventListener('click' function (e) {
+    const playerSelection = e.target.getAttribute('id');
+    game(playerSelection);
+    });
+};
+ 
+console.log(playerSelection)
 const point = document.createElement('div');
 if (winningNumber===0) {
     ++computerScore;
@@ -81,6 +92,7 @@ else if (winningNumber===1) {
 } else {
     point.textContent ="No one gains a point\n";
 }
+point.appendChild(container)
 
 let result = (playerScore<computerScore)
  ? "You lost! Try again?" 
