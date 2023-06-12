6136902c40131b4f33164f118d0f8dbeaaeeeef4
Iqra <iqrayasir2609@gmail.com>
2023-06-12 15:53:33 +0100

    Replace divs and rewrite functions

--- javascript.js
+++ javascript.js
@@ -13,59 +13,83 @@ function getComputerChoice(randomNumber) {
     }
 }
 
-function playRound(playerSelection,computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let lowerCase = playerSelection.toLowerCase();
     const declaration = document.createElement('div');
     if (lowerCase==="rock") {
         if (computerSelection==="rock") {
             declaration.textContent = "We tie! Rock equals rock";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 2;
         } else if (computerSelection==="paper") {
             declaration.textContent = "You lose! Paper beats rock";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 0;
         } else {
             declaration.textContent = "You win! Rock beats scissors";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 1;
         }
     } else if (lowerCase==="paper") {
         if (computerSelection==="paper") {
             declaration.textContent = "We tie! Paper equals paper";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 2;
         } else if (computerSelection==="scissors") {
             declaration.textContent = "You lose! Scissors beats paper";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 0;
         } else {
             declaration.textContent = "You win! Paper beats rock";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 1;
         }
     } else {
         if (computerSelection==="scissors") {
             declaration.textContent = "We tie! Scissors equals scissors";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 2;
         } else if (computerSelection==="rock") {
             declaration.textContent = "You lose! Rock beats scissors";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 0;
         } else {
             declaration.textContent = "You win! Scissors beats paper";
-            declaration.appendChild(container);
+            container.appendChild(declaration);
             return 1;
         }
     }
 } 
 
-function game(playerSelection) {
-    let randomNumber = Math.floor(Math.random()*3)+1;
-    console.log(playerSelection)
-    let computerSelection = getComputerChoice(randomNumber);
-    let winningNumber=playRound(playerSelection,computerSelection);
+function result(playerScore, computerScore) {
+    let result = (playerScore<computerScore)
+    ? "You lost! Try again?" 
+    : "You won! Rematch?"
+    const winner = document.createElement('div');
+    winner.textContent = `${result}`;
+    return winner;
+}
+
+function game(playerSelection, playerScore, computerScore, container) {
+    while ((computerScore < 5) || (playerScore < 5)) {
+        let randomNumber = Math.floor(Math.random()*3)+1;
+        let computerSelection = getComputerChoice(randomNumber);
+        let winningNumber = playRound(playerSelection, computerSelection, container);
+        const point = document.createElement('div');
+        if (winningNumber===0) {
+            ++computerScore;
+            point.textContent = "Your opponent gains a point\n";
+        }
+        else if (winningNumber===1) {
+            ++playerScore;
+            point.textContent = "You gain a point\n";
+        } else {
+            point.textContent = "No one gains a point\n";
+        }
+        container.appendChild(point)
+    }
+    const winner = result(playerScore, computerScore);
+    container.appendChild(winner);
 }
 
 //main program
@@ -74,27 +98,8 @@ let computerScore = 0;
 const container = document.querySelector('.container');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click' function (e) {
-    const playerSelection = e.target.getAttribute('id');
-    game(playerSelection);
+    button.addEventListener('click', function (e) {
+    const playerSelection = button.getAttribute('id');
+    game(playerSelection, playerScore, computerScore, container);
     });
-};
- 
-console.log(playerSelection)
-const point = document.createElement('div');
-if (winningNumber===0) {
-    ++computerScore;
-    point.textContent ="Your opponent gains a point\n";
-}
-else if (winningNumber===1) {
-    ++playerScore;
-    point.textContent ="You gain a point\n";
-} else {
-    point.textContent ="No one gains a point\n";
-}
-point.appendChild(container)
-
-let result = (playerScore<computerScore)
- ? "You lost! Try again?" 
- : "You won! Rematch?"
-winner.textContent = `${result}`
+});
