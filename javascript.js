e771cc599ba9af50f9a23a21ccc1db5efb757d5b
Iqra <iqrayasir2609@gmail.com>
2023-06-13 15:51:08 +0100

    Rewrite code, add multiple rounds, new dialogue

--- javascript.js
+++ javascript.js
@@ -62,44 +62,59 @@ function playRound(playerSelection, computerSelection) {
 } 
 
 function result(playerScore, computerScore) {
-    let result = (playerScore<computerScore)
-    ? "You lost! Try again?" 
-    : "You won! Rematch?"
+    let result = "";
+    if (playerScore<computerScore) {
+        result = "You lost! Try again?";
+    } else if (computerScore<playerScore) {
+        result = "You won! Rematch?";
+    } else {
+        result = "Draw";
+    }
     const winner = document.createElement('div');
     winner.textContent = `${result}`;
     return winner;
 }
 
 function game(playerSelection, playerScore, computerScore, container) {
-    while ((computerScore < 5) || (playerScore < 5)) {
-        let randomNumber = Math.floor(Math.random()*3)+1;
-        let computerSelection = getComputerChoice(randomNumber);
-        let winningNumber = playRound(playerSelection, computerSelection, container);
-        const point = document.createElement('div');
-        if (winningNumber===0) {
-            ++computerScore;
-            point.textContent = "Your opponent gains a point\n";
-        }
-        else if (winningNumber===1) {
-            ++playerScore;
-            point.textContent = "You gain a point\n";
-        } else {
-            point.textContent = "No one gains a point\n";
-        }
-        container.appendChild(point)
+    let randomNumber = Math.floor(Math.random()*3)+1;
+    let computerSelection = getComputerChoice(randomNumber);
+    let winningNumber = playRound(playerSelection, computerSelection, container);
+    const point = document.createElement('div');
+    if (winningNumber===0) {
+        ++computerScore;
+        point.textContent = "Your opponent gains a point\n";
     }
-    const winner = result(playerScore, computerScore);
-    container.appendChild(winner);
+    else if (winningNumber===1) {
+        ++playerScore;
+        point.textContent = "You gain a point\n";
+    } else {
+        point.textContent = "No one gains a point\n";
+    }
+    container.appendChild(point)
+    let winner = result(playerScore, computerScore);
+    return winner;
 }
 
 //main program
 let playerScore = 0;
 let computerScore = 0;
+const playerDisplay = document.createElement('div');
+const computerDisplay = document.createElement('div');
 const container = document.querySelector('.container');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', function (e) {
-    const playerSelection = button.getAttribute('id');
-    game(playerSelection, playerScore, computerScore, container);
-    });
+    while ((playerScore !== 5) || (computerScore !== 5)) {
+        button.addEventListener('click', function () {
+            let playerSelection = button.getAttribute('id');
+            let pointCounter = game(playerSelection, playerScore, computerScore, container);
+            if (pointCounter == "You lost! Try again?") {
+                ++computerScore;
+            } else if (pointCounter == "You won! Rematch?") {
+                ++playerScore;
+            } 
+            playerDisplay.textContent(`Player score: ${playerScore}`);
+            computerDisplay.textContent(`Computer score: ${computerScore}\n`);
+            });
+    }
+    container.appendChild(winner)
 });
